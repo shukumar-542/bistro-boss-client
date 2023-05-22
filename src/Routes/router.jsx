949b406101b7490ecbfa3dc27d31c3dbc891fb54
shukumar-542 <shukumar542@gmail.com>
2023-05-22 23:34:17 +0600

    bistro boss client heder footer and banner added

---
+++ src/Routes/router.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import MainLayouts from "../Layouts/MainLayouts";
+import Home from "../Pages/Home/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayouts></MainLayouts>,
+      children : [
+        {
+            path : '/',
+            element : <Home></Home>
+        }
+
+      ]
+    },
+  ]);
